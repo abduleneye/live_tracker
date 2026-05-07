a5e93278f14e77b0934ac268d9c2a76c8b0df6c7
Abdulhameed Abdulhakeem <abdulhakeemab15@gmail.com>
2026-05-07 09:47:52 +0100

    Submitted for review

--- lib/features/live_tracking/domain/repository/tracking_repo.dart
+++ lib/features/live_tracking/domain/repository/tracking_repo.dart
@@ -1,6 +1,4 @@
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-import 'package:live_tracker/features/live_tracking/presentation/state/tracking_state.dart';
-
 abstract class TrackingRepo{
 Stream<GeoPoint> startTracking(List<GeoPoint> path);
 
