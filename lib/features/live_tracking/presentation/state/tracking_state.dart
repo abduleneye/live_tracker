094c7bb074275dbe8e1072b9393eb211becca0db
Abdulhameed Abdulhakeem eneye <abdulhakeemab15@gmail.com>
2026-05-07 03:58:04 +0100

    Delete lib/features/live_tracking/presentation/state directory

--- lib/features/live_tracking/presentation/state/tracking_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-
-class TrackingState {
-  final GeoPoint currentLocation;
-
-  TrackingState({
-    required this.currentLocation,
-  });
-}
